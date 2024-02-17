cf464666cdf51458811cf1a7d88407f063e63343
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-02-17 15:24:15 -0600

    initial commit

---
+++ build/CMakeFiles/appbuilder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/appbuilder.dir/main.c.o"
+  "CMakeFiles/appbuilder.dir/main.c.o.d"
+  "appbuilder"
+  "appbuilder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/appbuilder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/appbuilder.dir/main.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/appbuilder.dir/main.c.o: \
+  /Users/bhorowitz/Documents/CProjects/RaylibParserV2/main.c \
+  /opt/homebrew/Cellar/raylib/4.5.0/include/raylib.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/13.1.6/include/stdarg.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/13.1.6/include/stdbool.h
